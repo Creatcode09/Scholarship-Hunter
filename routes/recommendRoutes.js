45da77f03b8d92af409b7514394b0a7836befdda
Creatcode09 <yadavashutosh09013@gmail.com>
2026-04-09 23:53:15 +0530

    backend

--- routes/recommendRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getRecommendationsForUser } = require("../controllers/recommendController");
-
-// GET /api/recommend/:userId → Get ranked scholarship recommendations
-router.get("/:userId", getRecommendationsForUser);
-
-module.exports = router;
